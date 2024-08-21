bd90e8791db14c788f98e65e97eb3daa4448c52a
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-20 19:04:55 -0500

    added bcryptjs password

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 const express = require("express");
+const cors = require("cors");
 const conn = require("./db/conn");
 const app = express();
-const setupUsers = require("./db/setupUsers");
+const bcrypt = require("bcryptjs");
 const User = require("./models/user.model");
 require("dotenv").config();
 
+const salt = bcrypt.genSaltSync(10);
+
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT;
 
 conn();
 
-app.get("/", (req, res) => {
-  res.send("Home");
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-app.get("/users", async (req, res) => {
-  try {
-    const createdUsers = await User.create(setupUsers);
-    res.json(createdUsers);
-  } catch (error) {
-    console.log(
-      `Something went wrong with connect to the database ${error.message}`
-    );
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  console.log(userDoc, "userDoc");
+
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  res.json(passOk);
+  if (passOk) {
+    //logged in
+  } else {
+    res.status(400).json("Wrong credentials");
   }
+});
+
+app.get("/", (req, res) => {
+  res.send("Home");
 });
 
 app.listen(PORT, () => {
