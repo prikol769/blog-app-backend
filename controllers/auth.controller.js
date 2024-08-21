c01cb044951e1370eb7ff4a351cf6c1240b4b3db
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-21 16:09:22 -0500

    added env jwt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,15 +2,12 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const salt = bcryptjs.genSaltSync(10);
-const secret = "asdsadsa5dsa5d5as45d4as4d5";
-
 export const register = async (req, res) => {
   const { username, password } = req.body;
   try {
     const userDoc = await User.create({
       username,
-      password: bcryptjs.hashSync(password, salt),
+      password: bcryptjs.hashSync(password, 10),
     });
     res.json(userDoc);
   } catch (err) {
@@ -25,13 +22,18 @@ export const login = async (req, res) => {
   const passOk = bcryptjs.compareSync(password, userDoc.password);
   if (passOk) {
     // logged in
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).json({
-        id: userDoc._id,
-        username,
-      });
-    });
+    jwt.sign(
+      { username, id: userDoc._id },
+      process.env.JWT_SECRET,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).json({
+          id: userDoc._id,
+          username,
+        });
+      }
+    );
   } else {
     res.status(400).json("wrong credentials");
   }
@@ -43,7 +45,7 @@ export const logout = (req, res) => {
 
 export const profile = (req, res) => {
   const { token } = req.cookies;
-  jwt.verify(token, secret, {}, (err, info) => {
+  jwt.verify(token, process.env.JWT_SECRET, {}, (err, info) => {
     if (err) throw err;
     res.json(info);
   });
