179f44ab0495894b19837fc113c8eda462edec14
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-21 15:41:46 -0500

    added auth route

--- index.js
+++ index.js
@@ -1,54 +1,22 @@
-const express = require("express");
-const cors = require("cors");
-const conn = require("./db/conn");
-const app = express();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("./models/user.model");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import conn from "./db/conn.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
-const salt = bcrypt.genSaltSync(10);
-const secret = "asdsadsa5dsa5d5as45d4as4d5";
+dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
 
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
-
-const PORT = process.env.PORT;
+app.use(cookieParser());
 
 conn();
 
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const userDoc = await User.create({
-      username,
-      password: bcrypt.hashSync(password, salt),
-    });
-    res.json(userDoc);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-  const userDoc = await User.findOne({ username });
-  console.log(userDoc, "userDoc");
-
-  const passOk = bcrypt.compareSync(password, userDoc.password);
-  if (passOk) {
-    // logged in
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).json({
-        id: userDoc._id,
-        username,
-      });
-    });
-  } else {
-    res.status(400).json("wrong credentials");
-  }
-});
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Home");
---
+++ routes/auth.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  login,
+  logout,
+  register,
+  profile,
+} from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/profile", profile);
+
+export default router;
