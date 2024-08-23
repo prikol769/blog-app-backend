f79829a3f29c607527b36a943e3408c0a95c6bf5
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-23 16:38:24 -0500

    added post model

---
+++ models/post.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      maxLength: 200,
+    },
+    summary: {
+      type: String,
+      required: true,
+      maxLength: 400,
+    },
+    category: {
+      type: String,
+      required: true,
+      maxLength: 15,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
