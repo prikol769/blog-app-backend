82483ee43a5dd9987391e42fd84aa8cfdc0bdcfb
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-26 17:23:37 -0500

    added Delete post By Id

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -59,3 +59,17 @@ export const getPosts = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deletePostById = async (req, res, next) => {
+  console.log("delete");
+
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this post"));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json({ message: "The post has been deleted" });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { create, getPostById, getPosts } from "../controllers/post.controller.js";
+import {
+  create,
+  getPostById,
+  getPosts,
+  deletePostById,
+} from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/", verifyToken, create);
 router.get("/:postId", getPostById);
 router.get("/", getPosts);
+router.delete("/:postId/:userId", verifyToken, deletePostById);
 
 export default router;
