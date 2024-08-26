3477569ed05e7e3a08101d6fcbcb4b4d00c9bd64
Mykyta Krumalenko <prikol769@gmail.com>
2024-08-26 13:59:41 -0500

    added getPostByID route

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { create, getPosts } from "../controllers/post.controller.js";
+import { create, getPostById, getPosts } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, create);
+router.get("/:postId", getPostById);
 router.get("/", getPosts);
 
 export default router;
